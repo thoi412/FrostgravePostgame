f978841f06908911d07187f02e4864febcda91c4
thoi412 <thoi412@gmail.com>
2024-02-02 14:47:48 -0700

    Update with things in different files.

--- Classes.py
+++ Classes.py
@@ -1,5 +1,5 @@
 import Data
-
+from Spells import magic_schools
 
 class Item:
     def __init__(self, name, purchase_price, sale_price, item_type, description):
@@ -188,3 +188,188 @@ class Apothecary(Figure):
         self.properties = ""
 
 
+class Archer(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Archer"
+        self.move = 6
+        self.fight = 1
+        self.shoot = 2
+        self.armour = 11
+        self.will = 0
+        self.health = 10
+        self.cost = 75
+        self.equipment = ("Bow", "Quiver", "Dagger", "Light Armour")
+        self.properties = ""
+
+
+class Crossbowman(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Crossbowman"
+        self.move = 6
+        self.fight = 1
+        self.shoot = 2
+        self.armour = 11
+        self.will = 0
+        self.health = 10
+        self.cost = 75
+        self.equipment = ("Crossbow", "Quiver", "Dagger", "Light Armour")
+        self.properties = ""
+
+
+class TreasureHunter(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Treasure Hunter"
+        self.move = 7
+        self.fight = 3
+        self.shoot = 0
+        self.armour = 11
+        self.will = 2
+        self.health = 12
+        self.cost = 100
+        self.equipment = ("Hand Weapon", "Dagger", "Light Armour")
+        self.properties = ""
+
+
+class Tracker(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Tracker"
+        self.move = 7
+        self.fight = 1
+        self.shoot = 2
+        self.armour = 11
+        self.will = 1
+        self.health = 12
+        self.cost = 100
+        self.equipment = ("Staff", "Bow", "Quiver", "Light Armour")
+        self.properties = ""
+
+
+class Knight(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Knight"
+        self.move = 5
+        self.fight = 4
+        self.shoot = 0
+        self.armour = 13
+        self.will = 1
+        self.health = 12
+        self.cost = 125
+        self.equipment = ("Hand Weapon", "Dagger", "Shield", "Heavy Armour")
+        self.properties = ""
+
+
+class Templar(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Templar"
+        self.move = 5
+        self.fight = 4
+        self.shoot = 0
+        self.armour = 12
+        self.will = 1
+        self.health = 12
+        self.cost = 125
+        self.equipment = ("Two-Handed Weapon", "Heavy Armour")
+        self.properties = ""
+
+
+class Ranger(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Ranger"
+        self.move = 7
+        self.fight = 2
+        self.shoot = 2
+        self.armour = 11
+        self.will = 2
+        self.health = 12
+        self.cost = 125
+        self.equipment = ("Bow", "Quiver", "Dagger", "Hand Weapon")
+        self.properties = ""
+
+
+class Barbarian(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Barbarian"
+        self.move = 6
+        self.fight = 4
+        self.shoot = 0
+        self.armour = 10
+        self.will = 3
+        self.health = 14
+        self.cost = 125
+        self.equipment = ("Two-Handed Weapon", "Dagger")
+        self.properties = ""
+
+
+class Marksman(Figure):
+    def __init__(self, name, figure_type, move, shoot, armour, will, max_health, cost, equipment, properties):
+        super().__init__(name, figure_type, move, shoot, armour, will, max_health)
+        self.cost = cost
+        self.equipment = equipment
+        self.properties = properties
+
+        self.figure_type = "Marksman"
+        self.move = 5
+        self.fight = 2
+        self.shoot = 2
+        self.armour = 12
+        self.will = 1
+        self.health = 10
+        self.cost = 125
+        self.equipment = ("Crossbow", "Quiver", "Hand Weapon", "Heavy Armour")
+        self.properties = ""
+
+
+standard_soldiers = [Thug, Thief, WarHound, Infantryman, ManAtArms, Apothecary]
+
+specialist_soldiers = [Archer, Crossbowman, TreasureHunter, Tracker, Knight, Templar, Ranger, Barbarian, Marksman]
+
+soldier_types = [standard_soldiers, specialist_soldiers]
+
+
+def create_warband():
+    wizard_name = input("What is the wizard's name?")
+    for schools in magic_schools:
+        print(f'{schools}: {magic_schools[schools]}')
+    wizard_school = input(f"What school does the {wizard_name} follow?")
+    apprentice_name = input(f"What is the name of {wizard_name}'s apprentice?")
+    # Continue from here
